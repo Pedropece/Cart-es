91fc31f818fb83eabaeed21bbad5220a458b3714
Pedropece <pedrokaiky193@gmail.com>
2024-11-27 10:34:52 -0300

    Add files via upload
    Cartões desenvolvido durante o curso da alura

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Programação',
+    'O que é Phyton?',
+    'Phyton é uma Linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é capital de São Paulo?',
+    'São Paulo'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Função é um bloco de código que executa uma tarefa'
+)
+
+criaCartao(
+    'Frances',
+    'Qual é o jeito informal de falar "TUDO BEM" em frances?',
+    'O jeito informal de falar "TUDO BEM" em frances é: Çava bien?'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'Brasilia'
+)
